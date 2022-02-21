7240253037725fe71ce74f7905488098f7dfd7ab
aii23 <a.ivlev.om@gmail.com>
2022-02-21 13:57:27 +0300

    Added etherscan verification

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,35 @@
 require("@nomiclabs/hardhat-waffle");
 require("@nomiclabs/hardhat-solhint");
+require("@nomiclabs/hardhat-etherscan");
 require('solidity-coverage');
 require('hardhat-docgen');
 require('dotenv').config();
-
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
- 	solidity: "0.8.1",
+ 	solidity: {
+    	version: "0.8.1",
+    	settings: {
+      		optimizer: {
+        		enabled: true,
+        		runs: 20000
+      		}
+    	}
+  	},
  	networks: {
   		hardhat: {
     		forking: {
       			url: `https://mainnet.infura.io/v3/${ process.env.INFURA_KEY }`,
     		}
   		}, 
-
+  		rinkeby: {
+  			url: `https://rinkeby.infura.io/v3/${ process.env.INFURA_KEY }`, 
+  			accounts: [ process.env.RINKEBY_PRIVATE_KEY ]
+  		}
+	},
+	etherscan: {
+		apiKey: process.env.ETHERSCAN_KEY
 	}
 };
---
+++ scripts/deploy.js
@@ -0,0 +1,32 @@
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contracts with the account:", deployer.address);
+
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const OptionContract = await ethers.getContractFactory("Option");
+  // back = deployer
+  const Option = await OptionContract.deploy(deployer.address);
+
+  console.log("Option address:", Option.address);
+
+  console.log("Wait 1 minute before verification");
+
+  await new Promise(r => setTimeout(r, 60000));
+
+  console.log("Verification started");
+
+  await hre.run("verify:verify", {
+    address: Option.address,
+    constructorArguments: [ deployer.address ],
+  });
+
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
